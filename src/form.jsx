ccbb9654200bf7b0608eff75d0f4f628d950e9d8
Matin KHD <matink714@gmail.com>
2023-10-19 14:45:10 +0330

    implement Input component in Form component

--- src/form.jsx
+++ src/form.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 
+import Input from "./input";
+
 const initialFormValue = {
   recordName: "",
   artistName: "",
@@ -21,12 +23,31 @@ function Form({ onSubmit }) {
   return (
     <>
       <form onSubmit={onFormSubmitHandler}>
-        <label htmlFor="artistName">ArtistName:</label>
-        <input
+        <Input
           type="text"
+          id="artistName"
+          labelText="Artist Name"
+          key="artistName"
           placeholder="enter artist name"
           name="artistName"
-          id="artistName"
+          onChange={onInputChangeHandler}
+        />
+        <Input
+          type="text"
+          id="recordName"
+          labelText="Record Name"
+          key="recordName"
+          placeholder="enter record name"
+          name="recordName"
+          onChange={onInputChangeHandler}
+        />
+        <Input
+          type="textarea"
+          id="description"
+          labelText="Description"
+          key="description"
+          placeholder="enter some description"
+          name="description"
           onChange={onInputChangeHandler}
         />
         <button type="submit">Add</button>
