6e19e4a2df81a5297d7c197770221e1e72ca6c6b
Matin KHD <matink714@gmail.com>
2023-10-19 13:53:11 +0330

    implement some form inputs and some function for handling the form

--- src/form.jsx
+++ src/form.jsx
@@ -1,7 +1,36 @@
+import { useState } from "react";
+
+const initialFormValue = {
+  recordName: "",
+  artistName: "",
+  description: "",
+};
 function Form() {
+  const [inputValue, setInputValue] = useState(initialFormValue);
+
+  function onFormSubmitHandler(e) {
+    e.preventDefault();
+    console.log(inputValue);
+  }
+  function onInputChangeHandler(e) {
+    setInputValue({
+      ...inputValue,
+      [e.target.name]: e.target.value,
+    });
+  }
   return (
     <>
-      <p>form works!</p>
+      <form onSubmit={onFormSubmitHandler}>
+        <label htmlFor="artistName">ArtistName:</label>
+        <input
+          type="text"
+          placeholder="enter artist name"
+          name="artistName"
+          id="artistName"
+          onChange={onInputChangeHandler}
+        />
+        <button type="submit">Add</button>
+      </form>
     </>
   );
 }
