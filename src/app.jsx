ce6ec2b6453f3904162d2dbdcf98037dd951529a
Matin KHD <matink714@gmail.com>
2023-10-19 13:10:58 +0330

    initialzing the app

---
+++ src/app.jsx
@@ -0,0 +1,11 @@
+import { Outlet, NavLink } from "react-router-dom";
+
+import Footer from "./footer";
+
+import "./app.css";
+
+function App() {
+  return <p>App works</p>;
+}
+
+export default App;
